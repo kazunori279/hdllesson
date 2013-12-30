833e76d1da98e51ef9fbf63cb0274ba7e71a4405
Kazunori Sato <sato@sth.co.jp>
2013-12-30 19:03:18 +0900

    Reordered insts_tests.

--- CPU32/src/inst_tests.s
+++ CPU32/src/inst_tests.s
@@ -26,85 +26,45 @@ main:
 
 	# multu
 	nop
-	mtlo    $0
-	mthi    $0
-	addi    $t0,$0,-1
-	addi    $t1,$0,-1
-	multu   $t0,$t1
+	mtlo	$0
+	mthi	$0
+	addi	$t0,$0,-1
+	addi	$t1,$0,-1
+	multu	$t0,$t1
 assert_multu_hi: # hi should be fffffffe
-	mfhi    $t2
-	addi    $t2,$t2,2
-	bne     $t2,$0,assert_multu_hi 
+	mfhi	$t2
+	addi	$t2,$t2,2
+	bne		$t2,$0,assert_multu_hi 
 assert_multu_lo: # lo should be 1
 	nop
-	mflo    $t2
-	addi    $t7,$0,1
-	bne     $t2,$t7,assert_multu_lo
+	mflo	$t2
+	addi	$t7,$0,1
+	bne		$t2,$t7,assert_multu_lo
 
 	# mult
 	nop
-	mtlo    $0
-	mthi    $0
-	addi    $t0,$0,-1
-	addi    $t1,$0,-1
-	mult    $t0,$t1
+	mtlo	$0
+	mthi	$0
+	addi	$t0,$0,-1
+	addi	$t1,$0,-1
+	mult	$t0,$t1
 assert_mult_hi:
-	mfhi    $t2
-	bne     $t2,$0,assert_mult_hi
+	mfhi	$t2
+	bne		$t2,$0,assert_mult_hi
 assert_mult_lo:
 	nop
-	mflo    $t2
-	addi    $t7,$0,1
-	bne     $t2,$t7,assert_mult_lo
+	mflo	$t2
+	addi	$t7,$0,1
+	bne		$t2,$t7,assert_mult_lo
 
-	# sll
+	# mtlo, mflo
 	nop
 	addi	$t0,$0,0x1
-	sll	$t1,$t0,0x1
-assert_sll:
-	addi	$t7,$0,0x2
-	bne	$t1,$t7,assert_sll
-
-	# srl
-	nop
-	addi	$t0,$0,0x2
-	srl	$t1,$t0,0x1
-assert_srl:
+	mtlo	$t0
+	mflo	$t1
+assert_mtlo:
 	addi	$t7,$0,0x1
-	bne	$t1,$t7,assert_srl
-
-	# sra
-	nop
-	addi	$t0,$0,0x0
-	addi	$t1,$0,0x1
-	sub	$t0,$t0,$t1
-	sra	$t2,$t0,0x1
-assert_sra:
-	bne	$t2,$t0,assert_sra
-
-	# sllv
-	nop
-	addi	$t0,$0,0x1
-	addi	$t1,$0,0x3
-	sllv	$t2,$t0,$t1
-assert_sllv:
-	addi	$t7,$0,0x8
-	bne	$t2,$t7,assert_sllv
-
-	# srav
-	nop
-	addi    $t0,$0,0x0
-	addi    $t1,$0,0x1
-	sub	$t0,$t0,$t1
-	addi	$t2,$0,0x3
-	srav    $t2,$t0,$t2
-assert_srav:
-	bne     $t2,$t0,assert_srav
-
-	# jr
-	# jalr
-	# syscall
-	# break
+	bne		$t1,$t7,assert_mtlo
 
 	# mthi, mfhi
 	nop
@@ -113,18 +73,56 @@ assert_srav:
 	mfhi	$t1
 assert_mthi:
 	addi	$t7,$0,0x1
-	bne	$t1,$t7,assert_mthi
+	bne		$t1,$t7,assert_mthi
 	
-	# mtlo, mflo
+	# sll
 	nop
-	addi    $t0,$0,0x1
-	mtlo    $t0
-	mflo    $t1
-assert_mtlo:
-	addi    $t7,$0,0x1
-	bne     $t1,$t7,assert_mtlo
+	addi	$t0,$0,0x1
+	sll		$t1,$t0,0x1
+assert_sll:
+	addi	$t7,$0,0x2
+	bne		$t1,$t7,assert_sll
 
-end:
+	# srl
+	nop
+	addi	$t0,$0,0x2
+	srl		$t1,$t0,0x1
+assert_srl:
+	addi	$t7,$0,0x1
+	bne		$t1,$t7,assert_srl
+
+	# sra
+	nop
+	addi	$t0,$0,0x0
+	addi	$t1,$0,0x1
+	sub		$t0,$t0,$t1
+	sra		$t2,$t0,0x1
+assert_sra:
+	bne		$t2,$t0,assert_sra
+
+	# sllv
+	nop
+	addi	$t0,$0,0x1
+	addi	$t1,$0,0x3
+	sllv	$t2,$t0,$t1
+assert_sllv:
+	addi	$t7,$0,0x8
+	bne		$t2,$t7,assert_sllv
+
+	# srav
+	nop
+	addi	$t0,$0,0x0
+	addi	$t1,$0,0x1
+	sub		$t0,$t0,$t1
+	addi	$t2,$0,0x3
+	srav	$t2,$t0,$t2
+assert_srav:
+	bne		$t2,$t0,assert_srav
+
+	# jr
+	# jalr
+	# syscall
+	# break
 
 	.set	macro
 	.set	reorder
